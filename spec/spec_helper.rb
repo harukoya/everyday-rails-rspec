152fc2797d17a942857fa2c0aeedd01f29f824e9
harukoya <ka.uni.stu@gmail.com>
2022-05-01 01:41:22 +0900

    update

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,15 +16,14 @@
 require 'capybara/rspec'
 require 'selenium-webdriver'
 
+# FIXME deprecation :browser_option
 Capybara.register_driver :headless_chrome do |app|
   options = ::Selenium::WebDriver::Chrome::Options.new
 
   options.add_argument('--headless')
   options.add_argument('--no-sandbox')
-  options.add_argument('--disable-dev-shm-usage')
-  options.add_argument('--window-size=1400,1400')
 
-  driver = Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+  driver = Capybara::Selenium::Driver.new(app, browser: :chrome, capabilities: [options])
 end
 
 RSpec.configure do |config|
