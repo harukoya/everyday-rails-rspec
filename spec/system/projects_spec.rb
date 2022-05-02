c7f4c7876023e561d5839c7e01b19544d385dd02
harukoya <ka.uni.stu@gmail.com>
2022-05-02 12:59:05 +0900

    ヘルパメソッドを使ってみる in system/project_spec.rb

--- spec/system/projects_spec.rb
+++ spec/system/projects_spec.rb
@@ -1,35 +1,64 @@
 require 'rails_helper'
 
 RSpec.describe "Projects", type: :system do
+  let(:user) { FactoryBot.create(:user) }
+  let(:project) { FactoryBot.create(:project, owner: user) }
+
   scenario "user creates a new project" do
-    user = FactoryBot.create(:user)
     sign_in user
-    visit root_path
 
     expect {
-      click_link "New Project"
-      fill_in "Name", with: "Test Project"
-      fill_in "Description", with: "Trying out Capybara"
-      click_button "Create Project"
+      go_to_create_new_project_page
+      insert_project_name "Test Project"
+      insert_project_description "Trying out Capybara"
+      submit_create_project
     }.to change(user.projects, :count).by(1)
 
+    expect_create_project "Test Project"
+  end
+
+  scenario "edit project" do
+    sign_in user
+    go_to_edit_project_page project.name
+
+    expect_edit_project_page project.name
+  end
+
+  def go_to_create_new_project_page
+    visit root_path
+    click_link "New Project"
+  end
+
+  def go_to_edit_project_page(project_name)
+    visit root_path
+    click_link project_name
+  end
+
+  def insert_project_name(name)
+    fill_in "Name", with: name
+  end
+
+  def insert_project_description(description)
+    fill_in "Description", with: description
+  end
+
+  def submit_create_project
+    click_button "Create Project"
+  end
+
+  def expect_create_project(project_name)
     aggregate_failures do
       expect(page).to have_content "Project was successfully created"
-      expect(page).to have_content "Test Project"
+      expect(page).to have_content project_name
       expect(page).to have_content "Owner: #{user.name}"
     end
   end
 
-  scenario "edit project" do
-    user = FactoryBot.create(:user)
-    project = FactoryBot.create(:project, owner: user)
-    sign_in user
-    visit root_path
-
-    click_link "#{project.name}"
-    
-    expect(page).to have_content "#{project.name}"
-    expect(page).to have_content "Owner: #{user.name}"
-    expect(page).to have_link "Edit"
+  def expect_edit_project_page(project_name)
+    aggregate_failures do
+      expect(page).to have_content project_name
+      expect(page).to have_content "Owner: #{user.name}"
+      expect(page).to have_link "Edit"
+    end
   end
 end
