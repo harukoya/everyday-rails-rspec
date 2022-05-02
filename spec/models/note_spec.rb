cf7c2e03cf1775dabe30429ec98267cf60679f7d
harukoya <ka.uni.stu@gmail.com>
2022-05-02 11:26:22 +0900

    task modelを追加 + letとlet!を使ってみる

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,30 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  before :each do
-    @user = User.create(
-      first_name: "Joe",
-      last_name:  "Tester",
-      email:      "joetester@example.com",
-      password:   "dottle-nouveau-pavillion"
-    )
-
-    @project = @user.projects.create(
-      name: "Test Project"
-    )
-  end
-
-  it "generates associated data from a factory" do
-    note = FactoryBot.create(:note)
-    puts "This note's project is #{note.project.inspect}"
-    puts "This note's user is #{note.user.inspect}"
-  end
+  let(:user) { FactoryBot.create(:user) }
+  let(:project) { FactoryBot.create(:project, owner: user) }
 
   it "is valid with a user, project, and message" do
     note = Note.new(
       message:  "This is a sample note.",
-      user:     @user,
-      project:  @project
+      user:     user,
+      project:  project
     )
     expect(note).to be_valid
   end
@@ -36,31 +20,35 @@ RSpec.describe Note, type: :model do
   end
 
   describe "search message for a term" do
-    before :each do
-      @note1 = @project.notes.create(
-        message:  "This is the first note.",
-        user:     @user
-      )
-      @note2 = @project.notes.create(
-        message:  "This is the second note.",
-        user:     @user
-      )
-      @note3 = @project.notes.create(
-        message:  "First, preheat the oven.",
-        user:     @user
-      )
-    end
+    let!(:note1) {
+      FactoryBot.create(:note,
+                       project: project,
+                       user: user,
+                       message: "This is the first note.")
+    }
+    let!(:note2) {
+      FactoryBot.create(:note,
+                       project: project,
+                       user: user,
+                       message: "This is the second note.")
+    }
+    let!(:note3) {
+      FactoryBot.create(:note,
+                       project: project,
+                       user: user,
+                       message: "First, preheat the oven.")
+    }
 
     context "when a match is found" do
       it "returns notes that match the search term" do
-        expect(Note.search("first")).to include(@note1, @note3)
-        expect(Note.search("first")).to_not include(@note2)
+        expect(Note.search("first")).to include(note1, note3)
       end
     end
 
     context "when no match is found" do
       it "returns an empty collection when no results are found" do
         expect(Note.search("message")).to be_empty
+        expect(Note.count).to eq 3
       end
     end
   end
